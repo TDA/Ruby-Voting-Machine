e04d6453ef94274feaaa0c8f969a8a52b6fa0b02
Sai Pc <schand31@asu.edu>
2015-12-20 23:22:20 +0530

    all hash methods

--- candidate.rb
+++ candidate.rb
@@ -1,8 +1,26 @@
 class Candidate
+
+  attr_accessor :age, :birthplace, :name, :occupation
+
   def initialize(name, options = {})
-    self.name = name
-    self.occupation = options.occupation
-    self.age = options.age
-    self.birthplace = options.birthplace
+    @name = name
+    @occupation = options[:occupation]
+    @age = options[:age]
+    @birthplace = options[:birthplace]
   end
 end
+
+# full verbose
+sai = Candidate.new("Sai", {:occupation => "Being Awesome", :age => 22, :birthplace => "Chennai"})
+p sai
+
+# with braces
+sai = Candidate.new("Sai", {occupation: "Being Awesome", age: 22, birthplace: "Chennai"})
+p sai
+
+# without braces
+sai = Candidate.new("Sai", occupation: "Being Awesome", age: 22, birthplace: "Chennai")
+p sai
+
+sai = Candidate.new("Sai")
+p sai
